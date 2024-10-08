7c9b304f84b463695116798c099a5d1196779848
Khodov <prophet322@mail.ru>
2024-10-08 21:58:43 +0300

    crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -1,11 +1,9 @@
 import sqlite3
 
 def initiate_db(db_name='products.db'):
-    # Подключаемся к базе данных (если файла базы данных не существует, он будет создан)
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
 
-    # Создаём таблицу Products, если она ещё не существует
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS Products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,25 +25,20 @@ def initiate_db(db_name='products.db'):
                    (f'{title[2]}', f'{description[2]}', f'{price[2]}'))
     cursor.execute('INSERT INTO Products(title, description, price) VALUES (?, ?, ?)',
                    (f'{title[3]}', f'{description[3]}', f'{price[3]}'))
-    # Сохраняем изменения и закрываем соединение с базой данных
     conn.commit()
     conn.close()
 
 def get_all_products(db_name='products.db'):
-    # Подключаемся к базе данных
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
 
-    # Выполняем SQL-запрос для получения всех записей из таблицы Products
     cursor.execute('SELECT * FROM Products')
     products = cursor.fetchall()
 
-    # Закрываем соединение с базой данных
     conn.close()
 
     return products
 
-# Пример использования функций
 if __name__ == '__main__':
     initiate_db()
     products = get_all_products()
